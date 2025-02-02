1c88a1b54e0fae4deeacd8348f0c6afb61d40647
Nagul75 <nagulk697@gmail.com>
2025-02-02 22:39:06 +0530

    made Cards clickable by wrapping anchor

--- Memory-Game/src/components/Card.jsx
+++ Memory-Game/src/components/Card.jsx
@@ -2,8 +2,10 @@ import "../styles/Card.css"
 
 export default function Card({children}) {
     return (
+        <a href="#" className="card-link">
         <div className="card-container">
         {children}
         </div>
+        </a>
     )
 }
